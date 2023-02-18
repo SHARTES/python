16d0e830b6d2726507d4cccecdeba86036f56ce0
SHARTES <artemshvets333@gmail.com>
2023-02-18 22:00:23 +0100

    testtest

--- test.py
+++ test.py
@@ -1,10 +1,16 @@
-print('hello world')
-e = open('1.txt').read().split()
-print(e)
-print('cassandra')
-for i in range(1,101):
-    print(i)
-calc = int(input())
-lucl = int(input())
-print(calc+lucl)
-print(calc*lucl)
+# print('hello world')
+# e = open('1.txt').read().split()
+# print(e)
+# print('cassandra')
+# for i in range(1,101):
+#     print(i)
+# calc = str(input())
+# lucl = str(input())
+# print(calc+lucl)
+# print(calc*100)
+e = []
+for i in range(5):
+    r = int(input())
+    e.append(r)
+# print(e[0]+e[1]+e[2]+e[3]+e[4])
+print(sum(e))
