b7558f1e0f7ab4a7ad62d3e243fa27fad89afcec
SHARTES <artemshvets333@gmail.com>
2023-02-20 19:46:34 +0100

    testtest

---
+++ 111.py
@@ -0,0 +1,17 @@
+# разделение строки на символы
+x = "hello"
+print(x[0]) # выведет "e"
+print(x[1])
+print(x[2])
+print(x[2])
+
+# добавление элемента в список
+x = [1, 2, 3, 4, 5]
+x.append(6)
+del x[2]
+print(x) # выведет [1, 2, 3, 4]
+
+x = {"name": "Artem","age": "17,turn 18 in a month","job": "student"}
+print(x["name"])
+print(x["age"])
+print(x["job"])
